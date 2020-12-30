d5b543c26f482db5450dd4f8c606b98fecf856cf
tli <tli@ubuntu>
2020-12-30 16:53:17 -0600

    add some basic settings setup

--- ubpsn_web/food_catalog/models.py
+++ ubpsn_web/food_catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class DriedBeans:
+    pass
+
+class CannedBeans:
+    pass
+
+class CannedVegetables:
+    pass
+
+class CannedOthers:
+    pass
+
+class Pasta:
+    pass
+
+class Misc:
+    pass
+
+class NutButtersJams:
+    pass
+
+class Snacks:
+    pass
+
+class BakingGoods:
+    pass
+
+class InstantCookFoods:
+    pass
+
+class SanitationProducts:
+    pass
+
+class FrozenFood:
+    pass
